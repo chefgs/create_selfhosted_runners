d5deec5e085e5ca5f4d5c8d11d7a7af7a691e7c2
Saravanan Gnanaguru <g.gsaravanan@gmail.com>
2024-02-26 00:11:34 +0530

    updated script for linux

--- get_repo_token.sh
+++ get_repo_token.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 # Check if both arguments are provided
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 1 ]; then
     echo "Missing arguments. Usage: $0 <repo_to_add_runner>"
     exit 1
 fi
 
 repo_to_add_runner=$1
-github_pat_token=${{ secrets.GH_PAT }}
 
 # Create GitHub runner registration token to be used in config.sh input
-curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $github_pat_token" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/chefgs/$repo_to_add_runner/actions/runners/registration-token | jq .token --raw-output # > token.txt
+curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/chefgs/$repo_to_add_runner/actions/runners/registration-token | jq .token --raw-output # > token.txt
 # repo_token=$(cat token.txt)
